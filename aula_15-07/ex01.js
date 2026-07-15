682b9b01fcccf7ce1e28f2c56cd3aa1fc086ece6
Leleh06 <leticyasilvad1608@gmail.com>
2026-07-15 17:38:14 -0300

    sla

---
+++ aula_15-07/ex01.js
@@ -0,0 +1,51 @@
+//Operação Ternario
+
+// let num = 17
+// let result
+
+ 
+
+// result= num % 2 == 0 ? `${num} é par` : `${num} é impar`
+// console.log(result);
+
+// if (num % 2 == 0) {
+//     console.log(`${num} é par`)
+// } else {
+//     console.log(`${num} é impar`);
+
+// }
+
+
+// let idade = Number(prompt("Qual a sua idade?"))
+
+// let maiorIdade
+
+// maiorIdade = idade >= 18 ? `Você é maior de Idade` : `Você é menor de idade`
+// alert(maiorIdade);
+
+// let num = 42
+// let divisivel
+
+// divisivel = num % 7 == 0 ? `${num} é multiplo de 7` : `${num} não é multiplo de 7`
+
+// console.log(divisivel);
+
+
+// let num = 1
+
+// while(num <= 10 ){
+//     console.log(++num)
+// }
+
+
+let num = 1
+
+while(num < 100){
+    num++
+    if(num % 2 == 1){
+        console.log(num);
+    }else{
+        continue
+    }
+    
+}
